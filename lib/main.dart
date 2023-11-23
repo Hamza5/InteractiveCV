8c116d347bf991f02c75abda14335c2a9c1f7b08
Hamza Abbad <hamza.abbad@gmail.com>
2023-11-23 15:02:05 +0800

    Layout the basic information.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const firstName = 'Hamza';
 const lastName = 'Abbad';
 const shortDescription = 'PhD student in computer science and artificial intelligence';
 const photoPath = 'images/Photo.jpg';
+const email = 'hamza.abbad@gmail.com';
+const phone1 = '🇨🇳 +86 139 7165 4983';
+const phone2 = '🇩🇿 +213 659 41 84 69';
+const city = 'Dalian';
+const province = 'Liaoning';
+const country = 'China';
+const nationality = '🇩🇿 Algerian';
+const religion = '☪️ Islam';
+const github = 'github.com/Hamza5';
+const linkedin = 'linkedin.com/in/hamza-abbad/';
+
 const tabTitles = [
   'Basic',
   'Education',
@@ -14,17 +26,17 @@ const tabIcons = [
 ];
 
 void main() {
-  runApp(const DynamicCV());
+  runApp(const InteractiveCV());
 }
 
-class DynamicCV extends StatelessWidget {
-  const DynamicCV({super.key});
+class InteractiveCV extends StatelessWidget {
+  const InteractiveCV({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dynamic CV',
+      title: 'Interactive CV',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         // useMaterial3: true,
@@ -51,13 +63,50 @@ class MainPage extends StatelessWidget {
           tabs: [for (var i=0; i<tabTitles.length; i++) Tab(text: tabTitles[i], icon: Icon(tabIcons[i]),)],
         ),
       ),
-      body: TabBarView(
-        children: [for (var _ in tabTitles) const Placeholder()],
+      body: const TabBarView(
+        children: [BasicInfo(), Placeholder()],
       ),
     );
   }
 }
 
+class InfoItem extends StatelessWidget {
+  final IconData iconName;
+  final String text;
+  const InfoItem({super.key, required this.iconName, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: InkWell(
+            onTap: () {},
+            child: ListTile(leading: FaIcon(iconName), title: Text(text))
+        )
+    );
+  }
+}
+
+
+class BasicInfo extends StatelessWidget {
+  const BasicInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 400, mainAxisExtent: 50),
+      children: const [
+        InfoItem(iconName: Icons.email, text: email),
+        InfoItem(iconName: Icons.phone, text: phone1),
+        InfoItem(iconName: Icons.phone, text: phone2),
+        InfoItem(iconName: Icons.location_pin, text: '$city, $province, $country'),
+        InfoItem(iconName: Icons.flag_circle, text: nationality),
+        InfoItem(iconName: Icons.book, text: religion),
+        InfoItem(iconName: FontAwesomeIcons.github, text: github),
+        InfoItem(iconName: FontAwesomeIcons.linkedin, text: linkedin),
+      ],
+    );
+  }
+}
 
 class CirclePhoto extends StatelessWidget {
   final ImageProvider photoProvider;

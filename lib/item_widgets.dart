c033f57f0ae39db66f87a10560192f47fbf090b8
Hamza Abbad <hamza.abbad@gmail.com>
2024-01-21 16:00:03 +0800

    Moved the OpenWeatherMap code into a separate API file.

--- lib/item_widgets.dart
+++ lib/item_widgets.dart
@@ -1,17 +1,15 @@
-import 'dart:convert';
-
 import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hijri/hijri_calendar.dart';
+import 'package:interactive_cv/api.dart';
 import 'package:intl/intl.dart' show DateFormat;
 import 'package:one_clock/one_clock.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class BasicInfoItem extends StatelessWidget {
@@ -83,24 +81,12 @@ class LocationItem extends StatelessWidget {
       icon: Icons.location_pin, title: fullAddress,
     );
     final weatherSection = FutureBuilder(
-      future: http.get(
-        Uri.parse(
-          'https://api.openweathermap.org/data/2.5/weather?lat=${geoPosition.latitude}&lon=${geoPosition.longitude}&'
-              'appid=$owmApiKey&units=metric&lang=${Localizations.localeOf(context).languageCode}',
-        ),
-      ).then((response) => json.decode(response.body) as Map<String, dynamic>),
+      future: OpenWeatherMap(geoLocation: geoPosition, lang: Localizations.localeOf(context).languageCode).getWeather(),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           return const SizedBox.shrink();
         } else if (snapshot.hasData) {
-          final currentWeather = snapshot.requireData;
-          final double windSpeed = (currentWeather['wind']['speed'] / 1000) / (1 / 3600);
-          final double windDegree = currentWeather['wind']['deg'];
-          final String iconName = currentWeather['weather'][0]['icon'];
-          final String description = currentWeather['weather'][0]['description'];
-          final double temperature = currentWeather['main']['temp'];
-          final double feelsLike = currentWeather['main']['feels_like'];
-          // final double humidity = currentWeather['main']['humidity'];
+          final w = snapshot.requireData;
           return Container(
               decoration: BoxDecoration(
                 borderRadius: const BorderRadiusDirectional.only(topEnd: Radius.circular(5)),
@@ -118,18 +104,18 @@ class LocationItem extends StatelessWidget {
                         alignment: Alignment.center,
                         children: [
                           Image.network(
-                            'https://openweathermap.org/img/wn/$iconName@2x.png', height: 66, width: 66,
+                            'https://openweathermap.org/img/wn/${w.iconName}@2x.png', height: 66, width: 66,
                             fit: BoxFit.fill, loadingBuilder: imageLoadingBuilder,
                             color: Theme.of(context).colorScheme.shadow.withOpacity(0.5),
                           ),
                           Image.network(
-                            'https://openweathermap.org/img/wn/$iconName@2x.png', height: 64, width: 64,
+                            'https://openweathermap.org/img/wn/${w.iconName}@2x.png', height: 64, width: 64,
                             fit: BoxFit.fill, loadingBuilder: imageLoadingBuilder,
                           ),
                         ],
                       ),
                       Text(
-                        description.characters.first.toUpperCase() + description.substring(description.characters.first.length),
+                        w.description.characters.first.toUpperCase() + w.description.substring(w.description.characters.first.length),
                         style: Theme.of(context).textTheme.titleMedium,
                       ),
                     ],
@@ -142,7 +128,7 @@ class LocationItem extends StatelessWidget {
                         children: [
                           const FaIcon(FontAwesomeIcons.temperatureHalf),
                           const SizedBox(width: 5),
-                          Text('${temperature.round()}°C', textDirection: TextDirection.ltr),
+                          Text('${w.temperature.round()}°C', textDirection: TextDirection.ltr),
                         ],
                       ),
                       Row(
@@ -150,18 +136,18 @@ class LocationItem extends StatelessWidget {
                         children: [
                           const Icon(Icons.face),
                           const SizedBox(width: 5),
-                          Text('${feelsLike.round()}°C', textDirection: TextDirection.ltr),
+                          Text('${w.feelsLike.round()}°C', textDirection: TextDirection.ltr),
                         ],
                       ),
                       Row(
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           Transform.rotate(
-                            angle: degToRadian(windDegree - 45),
+                            angle: degToRadian(w.windDegree - 45),
                             child: const FaIcon(FontAwesomeIcons.locationArrow),
                           ),
                           const SizedBox(width: 5),
-                          Text('${windSpeed.round()}km/h'),
+                          Text('${w.windSpeed.round()}km/h'),
                         ],
                       ),
                     ],
